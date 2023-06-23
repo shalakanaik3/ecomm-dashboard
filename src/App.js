051754796d73f1a4ee3c8b2c9511e745f450043d
shalakanaik3 <naik.shalaka99@gmail.com>
2023-06-23 12:30:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>E-COMMERCE PROJECT</h1>
+      <button>Normal Button</button>
+      <Button>bootstrap button</Button>
+    </div>
+  );
+}
+
+export default App;
